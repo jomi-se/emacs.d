b29f8668c61f5307c8c6b15f7c12f04d60f07f5d
Jose Miguel Arroyo <jose-miguel.arroyo@softathome.com>
2017-06-23 13:50:57 +0200

    Deactivate ivy and smex
    We're on team helm here (but keep conf around, you never know)

--- init.el
+++ init.el
@@ -64,8 +64,8 @@
 (require 'init-flycheck)
 
 (require 'init-recentf)
-(require 'init-smex)
-(require 'init-ivy)
+;(require 'init-smex)
+;(require 'init-ivy)
 (require 'init-hippie-expand)
 (require 'init-company)
 (require 'init-windows)
