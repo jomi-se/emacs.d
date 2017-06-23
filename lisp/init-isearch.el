beb4c9125998001d49593cc1f1292995e684c2e5
Jose Miguel Arroyo <jose-miguel.arroyo@softathome.com>
2017-06-23 14:14:49 +0200

    Keep default DEL key behaviour in isearch

--- lisp/init-isearch.el
+++ lisp/init-isearch.el
@@ -9,9 +9,6 @@
 (when (maybe-require-package 'emacs '(24 3))
   (define-key isearch-mode-map (kbd "C-c C-o") 'isearch-occur)) ;; to match ivy conventions
 
-;; DEL during isearch should edit the search string, not jump back to the previous result
-(define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char)
-
 ;; Search back/forth for the symbol at point
 ;; See http://www.emacswiki.org/emacs/SearchAtPoint
 (defun isearch-yank-symbol ()
