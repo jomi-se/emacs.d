bed2ceacafae62e3bc34b78f65116e0e9ef810a1
Jose Miguel Arroyo <jose-miguel.arroyo@softathome.com>
2017-06-23 14:12:52 +0200

    Remove M-? binding for ag

--- lisp/init-grep.el
+++ lisp/init-grep.el
@@ -7,8 +7,7 @@
 (when (and (executable-find "ag")
            (maybe-require-package 'ag))
   (require-package 'wgrep-ag)
-  (setq-default ag-highlight-search t)
-  (global-set-key (kbd "M-?") 'ag-project))
+  (setq-default ag-highlight-search t))
 
 (when (and (executable-find "rg")
            (maybe-require-package 'rg))
