e499b24b96f7714fc753cf00a918976104b80928
Jose Miguel Arroyo <jose-miguel.arroyo@softathome.com>
2017-06-23 14:03:02 +0200

    Add some newline funcs that I'm used to

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -65,7 +65,32 @@
   (move-end-of-line 1)
   (newline-and-indent))
 
-(global-set-key (kbd "S-<return>") 'sanityinc/newline-at-end-of-line)
+(defun jomi-se/vi-open-line-above ()
+  "Insert a newline above the current line and put point at beginning."
+  (interactive)
+  (unless (bolp)
+    (beginning-of-line))
+  (newline)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(defun jomi-se/vi-open-line-below ()
+  "Insert a newline below the current line and put point at beginning."
+  (interactive)
+  (unless (eolp)
+    (end-of-line))
+  (newline-and-indent))
+
+(defun jomi-se/vi-open-line (&optional abovep)
+  "Insert a newline below the current line and put point at beginning.
+With a prefix argument, insert a newline above the current line."
+  (interactive "P")
+  (if abovep
+      (vi-open-line-above)
+    (vi-open-line-below)))
+
+(global-set-key (kbd "C-<return>") 'jomi-se/vi-open-line)
+(global-set-key (kbd "C-S-<return>") 'jomi-se/vi-open-line-above)
 
 
 
