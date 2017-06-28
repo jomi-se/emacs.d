6daaaf92cad203e750511a942a3e2dcb26dbf1eb
Jomi <jose-miguel.arroyo@softathome.com>
2017-06-28 10:39:48 +0200

    Fix support for yasnippet

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -2,6 +2,7 @@
   (github-clone "https://github.com/AndreaCrotti/yasnippet-snippets.git" user-emacs-directory))
 
 (require-package 'yasnippet)
+(require 'yasnippet)
 (add-to-list 'yas-snippet-dirs "~/.emacs.d/yasnippet-snippets")
 (yas-global-mode 1)
 
