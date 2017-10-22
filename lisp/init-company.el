3f940bbe540afc7a385df4fde3dd13ed6c510ea0
Jose M Arroyo <jose.m.arroyo.se@gmail.com>
2017-10-22 15:48:28 +0200

    Don't 'complete on tab-always-indent
    There's some wierd interaction between helm and tab-always-indent when
    set to 'complete.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,7 +1,7 @@
 ;; WAITING: haskell-mode sets tags-table-list globally, breaks tags-completion-at-point-function
 ;; TODO Default sort order should place [a-z] before punctuation
 
-(setq tab-always-indent 'complete)
+(setq tab-always-indent t)
 (add-to-list 'completion-styles 'initials t)
 ;; Stop completion-at-point from popping up completion buffers so eagerly
 (setq completion-cycle-threshold 5)
