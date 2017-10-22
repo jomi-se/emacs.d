9252380fb6c3da2c9080543ea11d69b813be95ad
Jose M Arroyo <jose.m.arroyo.se@gmail.com>
2017-10-22 15:47:18 +0200

    Don't electric-pair in (html) sgml-mode

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -4,7 +4,18 @@
 (require-package 'tagedit)
 (after-load 'sgml-mode
   (tagedit-add-paredit-like-keybindings)
-  (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1))))
+  (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1)))
+  (add-hook 'sgml-mode-hook
+            (lambda ()
+              ;; Default indentation to 2, but let SGML mode guess, too.
+              (set (make-local-variable 'sgml-basic-offset) 2)
+              (sgml-guess-indent)))
+  (add-hook
+   'sgml-mode-hook
+   (lambda ()
+     (setq-local electric-pair-inhibit-predicate
+                 `(lambda (c)
+                    (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c)))))))
 
 (add-auto-mode 'html-mode "\\.\\(jsp\\|tmpl\\)\\'")
 
