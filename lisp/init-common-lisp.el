cfa5010122eff4ad5559300eec57de692ebb2444
Jose M Arroyo <jose.m.arroyo.se@gmail.com>
2017-10-23 16:12:58 +0200

    Fix typo

--- lisp/init-common-lisp.el
+++ lisp/init-common-lisp.el
@@ -5,7 +5,7 @@
                               (require 'slime)
                               (normal-mode))))
 
-(load (expand-file-name "~/quicklisp/slime-helper.el]"))
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
 ;; Replace "sbcl" with the path to your implementation
 (setq inferior-lisp-program "sbcl")
 
