bc63c3927641b6e131abb8a3a76c1c6bd6c40636
Jose M Arroyo <jose.m.arroyo.se@gmail.com>
2018-01-29 09:54:43 +0100

    [Rust] Add snippets, completion and flychecker

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,7 +1,11 @@
 (when (maybe-require-package 'rust-mode)
-  (when (maybe-require-package 'racer)
-    (add-hook 'rust-mode-hook #'racer-mode)
-    (add-hook 'racer-mode-hook #'eldoc-mode)))
-
+  (when (and (maybe-require-package 'racer)
+             (maybe-require-package 'flycheck-rust)
+             (maybe-require-package 'company-racer))
+    (maybe-require-package 'rust-snippets)
+    (add-hook 'rust-mode-hook 'racer-mode)
+    (add-hook 'racer-mode-hook 'eldoc-mode)
+    (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+    (add-hook 'rust-mode-hook #'flycheck-rust-setup)))
 
 (provide 'init-rust)
