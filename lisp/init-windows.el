fe2ed4b290cf3caa322d04181ce5aa0cc41c314f
Jose M Arroyo <jose.m.arroyo.se@gmail.com>
2018-11-20 16:47:57 +0100

    Remove unused window switching keybindings and indent file

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -100,39 +100,31 @@ Call a second time to restore the original window configuration."
   (bind-key* (kbd "M-S-<down>")  'windmove-down))
 
 
-(defun prev-window ()
-  (interactive)
-  (other-window -1))
-
-(bind-key* (kbd "C-;") 'other-window)
-(bind-key* (kbd "C-,") 'prev-window)
-
-
 ;; Switch from horizontal split to vertical split
 (defun toggle-window-split ()
   (interactive)
   (if (= (count-windows) 2)
       (let* ((this-win-buffer (window-buffer))
-         (next-win-buffer (window-buffer (next-window)))
-         (this-win-edges (window-edges (selected-window)))
-         (next-win-edges (window-edges (next-window)))
-         (this-win-2nd (not (and (<= (car this-win-edges)
-                     (car next-win-edges))
-                     (<= (cadr this-win-edges)
-                     (cadr next-win-edges)))))
-         (splitter
-          (if (= (car this-win-edges)
-             (car (window-edges (next-window))))
-          'split-window-horizontally
-        'split-window-vertically)))
-    (delete-other-windows)
-    (let ((first-win (selected-window)))
-      (funcall splitter)
-      (if this-win-2nd (other-window 1))
-      (set-window-buffer (selected-window) this-win-buffer)
-      (set-window-buffer (next-window) next-win-buffer)
-      (select-window first-win)
-      (if this-win-2nd (other-window 1))))))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
 
 (bind-key* (kbd "C-x |") 'toggle-window-split)
 (when (and *is-a-mac* (fboundp 'mac-next-tab-or-toggle-tab-bar))
