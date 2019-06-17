a2c108dbe3257112c6d94866104c851660b4c1fe
Jose M Arroyo <jose.m.arroyo.se@gmail.com>
2019-06-17 10:33:12 +0200

    Add indent-guide mode to yaml

--- lisp/init-yaml.el
+++ lisp/init-yaml.el
@@ -1,6 +1,7 @@
 (when (maybe-require-package 'yaml-mode)
   (add-auto-mode 'yaml-mode "\\.yml\\.erb\\'")
-  (add-hook 'yaml-mode-hook 'goto-address-prog-mode))
+  (add-hook 'yaml-mode-hook 'goto-address-prog-mode)
+  (add-hook 'yaml-mode-hook 'indent-guide-mode))
 
 
 (provide 'init-yaml)
