ad766ed5ebfb3178b929f71761d4fc43d24468a6
jomi-se <jose.m.arroyo.se@gmail.com>
2020-10-30 12:46:08 +0100

    Markdown setup

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -1,7 +1,9 @@
 (when (maybe-require-package 'markdown-mode)
   (add-auto-mode 'markdown-mode "\\.md\\.html\\'")
   (after-load 'whitespace-cleanup-mode
-    (push 'markdown-mode whitespace-cleanup-mode-ignore-modes)))
+    (push 'markdown-mode whitespace-cleanup-mode-ignore-modes))
+  (after-load 'markdown-mode
+    (add-hook 'markdown-mode-hook #'visual-line-mode)))
 
 
 (provide 'init-markdown)
